7f52d916087ac6a27a8da099c2701a4ff750da26
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-05-01 14:13:53 +0530

    Create script.js

---
+++ ImageSliderArray/script.js
@@ -0,0 +1,42 @@
+//Immediately invoked function expression - IIFE
+//It is anonymous function.
+(function () {
+    const pictures = [
+        "img1",
+        "img2",
+        "img3",
+        "img4",
+        "img5",
+        "img6"
+    ]
+   
+    let count = 0;
+    const imgContainer = document.querySelector('.img-container');
+
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            if (btn.classList.contains('btn-left')) {    
+                if (count == 0) count = pictures.length;          
+                count--;
+                console.log(count);
+               
+            }
+            else {
+                console.log(count);
+                
+                count++;
+                if (count == pictures.length ) {
+                    count = 0;
+                }
+            }
+          imgContainer.style.setProperty('--bg', `assets/${pictures[count]}.jpg`); 
+          imgContainer.style.background =  `url(assets/${pictures[count]}.jpg)`; 
+
+        })
+       
+    })
+
+   
+
+})();
