06bc0231f83432e7abc19706f1b16f5a4ef5d9b5
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-06-23 15:11:55 +0530

    Create widthHeight.js

---
+++ widthHeight.js
@@ -0,0 +1,6 @@
+1. dom.offsetWidth   , dom.offsetHeight
+2. dom.clientWidth   , dom.clientHeight
+3. dom.scrollWidth   , dom.scrollHeight
+4. dom.getBoundingClientRect().width , dom.getBoundingClientRect().height
+5. dom.style.width , dom.style.height
+6. window.getComputedStyle(dom).width , window.getComputedStyle(dom).height
